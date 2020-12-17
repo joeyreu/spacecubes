33bbbe4ee25eaa7b23b18e6e906a0b19d201748e
joeyreu <jo.reusing@gmail.com>
2020-12-17 03:02:40 -0500

    fixed shared/glfw

---
+++ shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/src/cocoa_init.m" "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/cocoa_init.m.o"
+  "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/src/cocoa_monitor.m" "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o"
+  "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/src/cocoa_window.m" "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/cocoa_window.m.o"
+  "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/src/context.c" "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/context.c.o"
+  "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/src/init.c" "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/init.c.o"
+  "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/src/input.c" "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/input.c.o"
+  "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/src/iokit_joystick.m" "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/iokit_joystick.m.o"
+  "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/src/mach_time.c" "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/mach_time.c.o"
+  "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/src/monitor.c" "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/src/nsgl_context.m" "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/nsgl_context.m.o"
+  "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/src/posix_tls.c" "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/posix_tls.c.o"
+  "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/src/window.c" "/Users/joeyreu/Desktop/CS488_Project/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/window.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
